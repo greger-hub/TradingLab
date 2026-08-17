e6c69c1dbf494b11f7d6f73d6f668cab6e6b52ad
Greger Myrbakk <greger@myrbakk.se>
2026-08-17 15:15:19 +0200

    Migrate report verification to Instrument model

--- test_reports.py
+++ test_reports.py
@@ -1,10 +1,16 @@
 from api import get_instrument
 from api import get_reports
 
+
 instrument = get_instrument("INVE B")
 
-reports = get_reports(instrument["insId"])
+if instrument is None:
+    print("Instrument hittades inte.")
+else:
+    reports = get_reports(instrument.id)
 
-print(type(reports))
-print(len(reports))
-print(reports[0])
+    print(type(reports))
+    print(len(reports))
+
+    if reports:
+        print(reports[0])
